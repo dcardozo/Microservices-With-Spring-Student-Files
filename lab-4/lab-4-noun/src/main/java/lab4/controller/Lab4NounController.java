f59bcf4592563d2695f42e725cd4f522c7b9da14
David Cardozo <david.cardozo@apifao.com>
2016-01-11 22:40:03 -0600

    Lab 4 - Create a Spring Cloud Eureka Server and Client

---
+++ lab-4/lab-4-noun/src/main/java/lab4/controller/Lab4NounController.java
@@ -0,0 +1,23 @@
+package lab4.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by david on 1/10/16.
+ */
+@RestController
+public class Lab4NounController {
+
+    @Value("${words}")
+    String words;
+
+    @RequestMapping("/")
+    public @ResponseBody String getWord() {
+        String[] wordArray = words.split(",");
+        int i = (int) Math.round(Math.random() * (wordArray.length - 1));
+        return wordArray[i];
+    }
+}
