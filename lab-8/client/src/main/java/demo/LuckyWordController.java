0d12043d274de9e2ca72be696f71f8409de54fd5
David Cardozo <david.cardozo@apifao.com>
2016-01-13 15:37:48 -0600

    Lab 8 - Using Spring Cloud Bus

--- lab-8/client/src/main/java/demo/LuckyWordController.java
+++ lab-8/client/src/main/java/demo/LuckyWordController.java
@@ -1,18 +1,32 @@
 package demo;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-// TODO: Add @ConfigurationProperties here.
+@RefreshScope
 public class LuckyWordController {
-	 
+
+    @Value("${wordConfig.luckyWord}")
 	String luckyWord;
+
+    @Value("${wordConfig.preamble}")
 	String preamble;
-	
+
+    String fullStatement;
+
+    @PostConstruct
+    public void init() {
+        fullStatement = preamble + ": " + luckyWord;
+    }
+
 	@RequestMapping("/lucky-word")
 	public String showLuckyWord() {
-		return preamble + ": " + luckyWord;
+		return fullStatement;
 	}
 
 	public String getLuckyWord() {
